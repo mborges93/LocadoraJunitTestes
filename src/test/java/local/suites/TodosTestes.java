e076fc73c573cc5c005fb991ec34ae21a76d7153
MatheusBorges <matheusadsti@gmail.com>
2018-10-08 20:49:09 -0300

    Criando a classe TodosTestes

---
+++ src/test/java/local/suites/TodosTestes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package local.suites;
+
+import local.model.FilmeTest;
+import local.service.LocacaoServiceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+/**
+ *
+ * @author 781410047
+ */
+@RunWith(Suite.class)
+@SuiteClasses({LocacaoServiceTest.class})
+public class TodosTestes {
+    
+
+}
